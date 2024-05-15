74577e3fb05ceb026191e82771264e9ff8009cba
hy <10488351+huang_sheng_yi@user.noreply.gitee.com>
2024-05-16 00:43:36 +0800

    初始化底部导航栏 init Tab-Bar

---
+++ src/views/homePage.vue
@@ -0,0 +1,78 @@
+<template>
+  <el-card class="tab-bar">
+    <el-tabs v-model="activeNav" tab-position="bottom" :stretch="true">
+      <el-tab-pane name="first">
+        <template #label>
+          <el-icon>
+            <Opportunity />
+          </el-icon>
+          <span>说明</span>
+        </template>
+      </el-tab-pane>
+
+      <el-tab-pane name="second">
+        <template #label>
+          <el-icon>
+            <Opportunity />
+          </el-icon>
+          <span>说明</span>
+        </template>
+      </el-tab-pane>
+
+      <el-tab-pane name="third">
+        <template #label>
+          <el-icon>
+            <Opportunity />
+          </el-icon>
+          <span>说明</span>
+        </template>
+      </el-tab-pane>
+
+      <el-tab-pane name="fourth">
+        <template #label>
+          <el-icon>
+            <Opportunity />
+          </el-icon>
+          <span>说明</span>
+        </template>
+      </el-tab-pane>
+    </el-tabs>
+  </el-card>
+</template>
+
+<script>
+import { Opportunity } from "@element-plus/icons-vue";
+
+export default {
+  name: "homePage",
+  components: { Opportunity },
+  data() {
+    return {
+      activeNav: 'first',
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.tab-bar {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 999;
+
+  :deep(.el-card__body) {
+    padding: 0;
+  }
+
+  :deep(.el-tabs__header) {
+    margin-top: 0;
+  }
+
+  :deep(.el-tabs__item) {
+    padding: 0;
+    height: 60px;
+  }
+}
+</style>
