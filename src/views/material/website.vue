cdfced4bec7076fc295e8fdf92cd739d76166f08
MT-starful <598937469@qq.com>
2024-05-26 21:37:24 +0800

    完成推荐视频与推荐网站页 Complete the development of the Video page and Website page

--- src/views/material/website.vue
+++ src/views/material/website.vue
@@ -1,12 +1,26 @@
 <template>
-  推荐网站
+  <div v-for="series in seriesList" :key="series.title">
+    <SeriesTitle :title="series.title" />
+    <LinkCard v-for="linkInfo in series.linkList" :key="linkInfo.url" :linkInfo="linkInfo" />
+  </div>
 </template>
 
 <script>
+import LinkCard from "@/components/linkCard.vue";
+import SeriesTitle from "@/components/seriesTitle.vue";
+import { seriesList } from "@/../public/data/material/website.js";
+
 export default {
   name: 'websiteMaterial',
+  computed: {
+    seriesList() {
+      return seriesList
+    }
+  },
+  components: { SeriesTitle, LinkCard }
 }
 </script>
 
 <style scoped lang="scss">
+
 </style>
