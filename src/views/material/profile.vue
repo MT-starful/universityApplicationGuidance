053228bbe60a333e4fd5cc075cfeaca752165e42
MT-starful <598937469@qq.com>
2024-05-29 22:20:43 +0800

    完成资料栏规划 Complete the Profile page

--- src/views/material/profile.vue
+++ src/views/material/profile.vue
@@ -1,5 +1,21 @@
 <template>
-  辅导资料
+  <div class="btn-list">
+    <el-button class="btn" type="primary">防诈（含真实案例，准大学生必看）</el-button>
+  </div>
+
+  <div class="btn-list">
+    <el-button class="btn" type="primary">录取流程详解</el-button>
+    <el-button class="btn" type="primary">高考天地怎么看</el-button>
+  </div>
+
+  <div class="btn-list">
+    <el-button class="btn" type="primary">填报辅导与名词解释</el-button>
+    <el-button class="btn" type="primary">常见问题解答</el-button>
+  </div>
+
+  <div class="btn-list">
+    <el-button class="btn" type="primary">野鸡大学名单</el-button>
+  </div>
 </template>
 
 <script>
@@ -9,5 +25,12 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.btn-list {
+  display: flex;
+  margin-top: 24px;
 
+  .btn {
+    width: 100%;
+  }
+}
 </style>
